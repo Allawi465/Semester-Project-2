9ba9f284cbded04e78ae015c595527fb779f4e45
Allawi <allawi465@gmail.com>
2022-11-26 04:01:54 +0100

    update register function

---
+++ src/js/api/auth/register.mjs
@@ -0,0 +1,40 @@
+import { API_SOCIAL_URL } from "../constants.mjs";
+export const message = document.querySelector(".error");
+
+const signInModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('loginModel')) 
+const registerModal = bootstrap.Modal.getOrCreateInstance(document.getElementById('registerModel')) 
+
+const path = "/auth/register";
+const method = "POST";
+
+/**
+ * Register through api with post method  
+ * @param {register} api call with post method
+*/
+
+export async function register(user) {
+    const registerApiUrl = API_SOCIAL_URL + path;
+    const body = JSON.stringify(user);
+    try {
+        const response = await fetch(registerApiUrl, {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            method,
+            body
+        });
+
+        if(response.status >= 400) {
+            throw error;
+        };
+    
+        const data = await response.json();
+
+        if(data.status !== 201) {
+          signInModal.show();
+          registerModal.hide();
+        };
+    } catch(error) {
+        message.innerHTML = "Profile already exists";
+    }
+};
