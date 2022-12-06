60f3a613a7e68e01a5ca22a98c27eac4fd3db4ca
Allawi <allawi465@gmail.com>
2022-12-06 22:34:37 +0100

    api call addBid to listings

---
+++ src/js/api/auction/bid.mjs
@@ -0,0 +1,43 @@
+import { API_SOCIAL_URL } from '../constants.mjs';
+import { fetchWToken } from '../headers.mjs';
+
+const message = document.querySelector('.error-bid');
+// const closeBtn = document.querySelectorAll('.close');
+
+const queryStringPostId = document.location.search;
+const parameters = new URLSearchParams(queryStringPostId);
+const id = parameters.get('id');
+
+const path = `/listings/${id}/bids`;
+const method = 'POST';
+
+export async function MakeABid(amount) {
+  try {
+    const url = `${API_SOCIAL_URL}${path}`;
+    const response = await fetchWToken(url, {
+      method,
+      body: JSON.stringify(amount),
+    });
+
+    if (response.status >= 400) {
+      throw error;
+    }
+
+    const data = await response.json();
+
+    if (data.status !== 200) {
+      location.reload();
+    }
+  } catch (error) {
+    message.innerHTML = `<p class="text-center fw-semibold error"> "You do not have enough balance to bid this amount"</p>`;
+  }
+}
+
+/* closeBtn.forEach(close => [
+  close.addEventListener("click", hideErrorMessage)
+]); */
+
+/* 
+function hideErrorMessage() {
+  message.innerHTML = '';
+}  */
