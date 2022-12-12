0549d7060913a2344da6e84f5adc1c14a373e7a2
Allawi <allawi465@gmail.com>
2022-12-12 02:47:58 +0100

    updating profile function to render listings and bets

--- src/js/listeners/profile/index.mjs
+++ src/js/listeners/profile/index.mjs
@@ -5,16 +5,60 @@ import {
 } from '../../api/profiles/index.mjs';
 import * as templates from '../../templates/index.mjs';
 
+import { containerViewLists } from '../auction/view.mjs';
+
+import { spinner } from '../auction/view.mjs';
+
 const container = document.querySelector('.profile-card');
+const containerBets = document.querySelector('.renderBets');
 
 export async function viewProfile() {
   const meProfile = await getProfile();
 
   const listings = await profileListings();
 
-  const bids = await profileBids();
+  const bets = await profileBids();
 
-  console.log(bids, listings);
+  const options = { year: 'numeric', month: 'long', day: 'numeric' };
+
+  const optionsWithTime = {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  };
+
+  const sorterDate = listings.map((listing) => {
+    return {
+      ...listing,
+      created: new Date(listing.created).toLocaleDateString('no-NO', options),
+      endsAt: new Date(listing.endsAt).toLocaleDateString(
+        'no-NO',
+        optionsWithTime
+      ),
+    };
+  });
+
+  const sorterBets = bets.map((bet) => {
+    return {
+      ...bet,
+      created: new Date(bet.created).toLocaleDateString(
+        'no-NO',
+        optionsWithTime
+      ),
+    };
+  });
+
+  spinner.classList.remove('spinner-grow');
+
+  if (listings.length === 0) {
+    containerViewLists.innerHTML = 'No listings yet';
+  } else if (bets.length === 0) {
+    containerBets.innerHTML = 'No bets yet';
+  }
 
   templates.profileTemplate(meProfile, container);
+  templates.renderListings(sorterDate, containerViewLists);
+  templates.renderBets(sorterBets, containerBets);
 }
