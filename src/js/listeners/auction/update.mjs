3a7582b5b55e79b483851b7f35edf58204e4131d
Allawi <allawi465@gmail.com>
2022-12-13 20:30:42 +0100

    adding function to render out listing and update it

---
+++ src/js/listeners/auction/update.mjs
@@ -0,0 +1,31 @@
+import { containerViewLists } from './view.mjs';
+import { listingsById } from '../../api/auction/viewById.mjs';
+import { update } from '../../api/auction/update.mjs';
+
+export function editAListings() {
+  containerViewLists.addEventListener('click', async (e) => {
+    const editBtn = e.target.id === 'editPost';
+    let id = e.target.dataset.id;
+    const form = document.querySelector('#UpdateListing');
+
+    if (editBtn) {
+      if (form) {
+        const { tags, ...listing } = await listingsById(id);
+        console.log(tags);
+        form.title.value = listing.title;
+        form.tags.value = tags;
+        form.description.value = listing.description;
+        form.addEventListener('submit', (event) => {
+          event.preventDefault();
+          const form = event.target;
+          const formData = new FormData(form);
+          const listing = Object.fromEntries(formData.entries());
+          let newTags = listing.tags.replace(/\s+/g, '').split(',');
+          listing.tags = newTags;
+          listing.id = id;
+          update(listing);
+        });
+      }
+    }
+  });
+}
