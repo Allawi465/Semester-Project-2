c290d8a012450fbe40f9c4cdbcaf05906ff7b306
Allawi <allawi465@gmail.com>
2022-12-13 20:18:19 +0100

    adding folder delete and update to index.mjs

--- src/js/listeners/index.mjs
+++ src/js/listeners/index.mjs
@@ -6,6 +6,8 @@ export * from './auth/logOut.mjs';
 export * from './auction/view.mjs';
 export * from './auction/bid.mjs';
 export * from './auction/addItem.mjs';
+export * from './auction/delete.mjs';
+export * from './auction/update.mjs';
 
 export * from './settings/avatar.mjs';
 export * from './auction/viewById.mjs';
