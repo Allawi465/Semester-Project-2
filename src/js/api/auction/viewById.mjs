e7384c476367616798c0a24e51438e1d0d2ee4a4
Allawi <allawi465@gmail.com>
2022-12-13 21:01:06 +0100

    get by id function

--- src/js/api/auction/viewById.mjs
+++ src/js/api/auction/viewById.mjs
@@ -6,17 +6,33 @@ const queryStringPostId = document.location.search;
 const parameters = new URLSearchParams(queryStringPostId);
 const id = parameters.get('id');
 
-const path = `/listings/${id}`;
-
 /**
  * view posts content with api get method
- * @param {get} get posts content
+ * @param {get} get listings content by id
  * @param {fetchWToken} token from a function
  */
 
 export async function viewById() {
+  const path = `/listings/`;
   try {
-    const getUrl = `${API_SOCIAL_URL}${path}?_seller=true&_bids=true`;
+    const getUrl = `${API_SOCIAL_URL}${path}${id}?_seller=true&_bids=true`;
+    const response = await fetchWToken(getUrl, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    return await response.json();
+  } catch (error) {
+    errorMessage.style.display = 'block';
+    errorMessage.innerHTML = `<p class="text-center fw-semibold">we are aware of the issues with accessing NOxB, our team is actively working on it.</p>`;
+  }
+}
+
+export async function listingsById(id) {
+  const path = `/listings/`;
+  try {
+    const getUrl = `${API_SOCIAL_URL}${path}${id}?_seller=true&_bids=true`;
     const response = await fetchWToken(getUrl, {
       headers: {
         'Content-Type': 'application/json',
