3e48d900a93ee8633f0f568051c93dd1738af233
Allawi <allawi465@gmail.com>
2022-12-15 20:36:19 +0100

    updating newBalance when user bid

--- src/js/listeners/auction/bid.mjs
+++ src/js/listeners/auction/bid.mjs
@@ -2,16 +2,15 @@ import * as localStorage from '../../storage/index.mjs';
 import { MakeABid } from '../../api/auction/bid.mjs';
 
 /**
- * deleting a post by id
- * @param {containerForPosts} using the card posts to get the id from
- * @param {deleting} sending to api call
+ * bid on listing by id
+ * @param {credits} using the card listing to get the id from
+ * @param {formData} making new form to update the post by id
  */
-
 export function makeBid() {
   const form = document.getElementById('makeBid');
   const credits = localStorage.load('credits');
   if (form) {
-    form.addEventListener('submit', (event) => {
+    form.addEventListener('submit', async (event) => {
       event.preventDefault();
       const form = event.target;
       const formData = new FormData(form);
@@ -19,11 +18,12 @@ export function makeBid() {
       Object.keys(bidAmount).forEach((numb) => {
         bidAmount[numb] = parseInt(bidAmount[numb]);
       });
-      MakeABid(bidAmount);
-      console.log(bidAmount);
+      const response = await MakeABid(bidAmount);
+      if (response === true) {
+        const newBalance = credits - bidAmount.amount;
+        localStorage.save('credits', newBalance);
+      }
       form.reset();
-      const newBalance = credits - bidAmount.amount;
-      localStorage.save('credits', newBalance);
     });
   }
 }
