5c59d1e3cd84b5696d15c285afd968c1417122f1
Allawi <allawi465@gmail.com>
2022-12-15 20:42:16 +0100

    added JSDocs

--- src/js/api/auth/logout.mjs
+++ src/js/api/auth/logout.mjs
@@ -1,15 +1,14 @@
 import * as localStorage from '../../storage/index.mjs';
 
 /**
- * Checking Auth from localStorage
- * @param {loadTheValue} localStorage get value
- * @param {authValidation} if user true or false validation
- * @param {removeFromStorage} localStorage remove value
+ * clear localStorage when user log out
+ * @param {remove} localStorage remove value
  */
 
 export function logout() {
   localStorage.remove('profile');
   localStorage.remove('token');
+  localStorage.remove('credits');
   location.reload();
   location.href = '/NOxB/home/';
 }
