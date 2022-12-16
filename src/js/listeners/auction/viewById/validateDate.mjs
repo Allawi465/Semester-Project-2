9347e7da171642fc687c513839bee66fdb9b4e9f
Allawi <allawi465@gmail.com>
2022-12-16 20:45:35 +0100

    removing error

--- src/js/listeners/auction/viewById/validateDate.mjs
+++ src/js/listeners/auction/viewById/validateDate.mjs
@@ -1,17 +1,14 @@
-import * as localStorage from '../../../storage/index.mjs';
 import { optionsWithTime } from '../viewById.mjs';
 
 /**
  * validate date if is in the past
  * @param {thePast} todays date
  * @param {listings} endsAt date
- * @param {name} if listing author === name it hide bid button
  */
 
-export function validateDate(arg, author) {
+export function validateDate(arg) {
   const bidOnBtn = document.querySelector('#bidOn');
   const endTime = document.querySelector('.endTime');
-  const { name } = localStorage.load('profile');
   const NoBidsTexts = document.querySelector('.NobidsTexts');
   if (thePast(new Date(arg)) === true) {
     const endedDate = new Date(arg).toLocaleDateString(
@@ -21,10 +18,6 @@ export function validateDate(arg, author) {
     endTime.innerHTML = `Ended ${endedDate}`;
     bidOnBtn.style.display = 'none';
     NoBidsTexts.innerHTML = 'No bids were made';
-  }
-  if (author === name) {
-    bidOnBtn.style.display = 'none';
-    NoBidsTexts.innerHTML = 'No bids yet';
   }
 }
 
