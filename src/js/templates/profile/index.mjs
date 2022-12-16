1bb18ead6c46c07d62aea95c915e5465d1afd889
Allawi <allawi465@gmail.com>
2022-12-16 19:04:34 +0100

    adding more html to the templates

--- src/js/templates/profile/index.mjs
+++ src/js/templates/profile/index.mjs
@@ -31,8 +31,8 @@ export function TemplateProfile(profile) {
                         <strong>Listings</strong>
                     </div>
                     <div class="col col fs-6">
-                        <p class="mb-1">${profile.wins.length}</p>
-                        <strong>Bets</strong>
+                        <p class="mb-1 bids-length"></p>
+                        <strong>Bids</strong>
                     </div>
                     <div class="col col fs-6">
                         <p class="mb-1">${profile.wins.length}</p>
@@ -92,12 +92,12 @@ export function viewProfileListing(listings) {
               </div>
               <div class="card-body">
                   <div class="d-flex justify-content-between">
-                      <div> 
-                          <h5 class="me-2">${listings.title}</h5>
+                      <div style="max-width: 250px;"> 
+                          <h5 class="card-title text-break me-2 card-text-listings" style="max-width: 200px;">${listings.title}</h5>
                       </div>
                       <div class="d-flex">
-                          <p class="badge text-uppercase bg-warning mb-2 me-1 bids-list-badge"><span class="bids-list-text" style="color: #212529;">BIDS</span></p>
-                          <span class=" fw-bolder">${listings._count.bids}</span>
+                        <p class="badge text-uppercase bg-warning me-2 bids-list-badge" style="max-height: 25px;"><span class="bids-list-text" style="color: #212529;">BIDS</span></p>
+                        <span class=" fw-bolder">${listings._count.bids}</span>
                       </div>
                   </div>
               </div>
@@ -119,9 +119,10 @@ export function viewProfileListing(listings) {
  */
 
 export function renderListings(listings, parent) {
-  listings.forEach((listings) => {
-    parent.innerHTML += viewProfileListing(listings);
-  });
+  for (let i = 0; i < listings.length; i++) {
+    parent.innerHTML += viewProfileListing(listings[i]);
+    if (listings[i] === listings[10]) break;
+  }
 }
 
 /**
@@ -148,7 +149,7 @@ export function viewProfileBets(listings) {
               <div class="card-body">
                   <div class="d-flex justify-content-between">
                       <div> 
-                          <h5 class="me-2">Bid: ${listings.amount} <svg xmlns="http://www.w3.org/2000/svg"
+                          <h5 class="me-2">Bid amount: ${listings.amount} <svg xmlns="http://www.w3.org/2000/svg"
                           width="16" height="16" class="mb-1 me-1" fill="currentColor" class="bi bi-coin" viewBox="0 0 16 16">
                           <path
                               d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z" />
@@ -180,4 +181,57 @@ export function renderBets(listings, parent) {
     parent.innerHTML += viewProfileBets(listings[i]);
     if (listings[i] === listings[10]) break;
   }
+}
+
+/**
+ * view profile wins html as return from the function
+ * @param {return} returning html post div
+ */
+
+export function viewProfileWins(listings) {
+  return ` 
+        <div class="col my-2">
+            <div class="card shadow-sm bg-dark text-white shadow-lg p-3 mb-2 rounded profile-card-listings" style="border: none;">
+                <div class="card-header d-flex justify-content-between" style="flex-wrap: wrap;">
+                    <a type="button" class="link-profile text-white" href="/NOxB/profiles/?name=${listings.seller.name}" style="text-decoration: none";> 
+                        <div class="d-flex flex-row align-items-center" >
+                            <div
+                                class="rounded-circle overflow-hidden d-flex justify-content-center align-item-center me-2">
+                                <img src="${listings.seller.avatar}"  width="50" height="50" alt=""
+                                    class="rounded-circle profile-images-topBar">
+                            </div>
+                            <span class="font-weight-bol text-capitalize">${listings.seller.name}</span>
+                        </div>
+                    </a>
+                    <p class="card-text"><small class="createdDate fs-6" style="color: #bac8d5;"></small></p>
+                </div>
+                <div class="card-body">
+                    <div class="d-flex justify-content-between">
+                        <div style="max-width: 250px;"> 
+                            <h5 class="card-title me-2 card-text-listings">${listings.title}</h5>
+                        </div>
+                        <div class="d-flex">
+                            <p class="badge text-uppercase bg-warning me-2 bids-list-badge" style="max-height: 25px;"><span class="bids-list-text" style="color: #212529;">BIDS</span></p>
+                            <span class=" fw-bolder">${listings._count.bids}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-footer mb-2">
+                    <div class="d-flex justify-content-between align-items-center" style="flex-wrap: wrap;">
+                        <a type="button" class="btn btn-outline-light mb-2"
+                        href="/NOxB/auction/item/?id=${listings.id}">View</a>   
+                        <small class="fs-6 fw-bold endTimes" style="color: #bac8d5;"></small>
+                    </div>
+                </div>
+            </div>
+          </div>`;
+}
+
+/**
+ * Template to render html listings with a function
+ * @param {viewProfileWins} getting the html return from a function
+ */
+
+export function profileTemplateWins(listings, parent) {
+  parent.innerHTML += viewProfileWins(listings);
 }
