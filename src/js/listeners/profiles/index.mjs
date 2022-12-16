7a489aa681d65c1e6f912014dd5ca3a135429fb3
Allawi <allawi465@gmail.com>
2022-12-16 20:59:40 +0100

    removing else

--- src/js/listeners/profiles/index.mjs
+++ src/js/listeners/profiles/index.mjs
@@ -35,39 +35,40 @@ export async function viewProfiles() {
 
   if (listings.length === 0) {
     containerViewLists.innerHTML = 'No listings yet';
-  } else {
-    const sorterDate = listings.map((listing) => {
-      return {
-        ...listing,
-        created: new Date(listing.created).toLocaleDateString('no-NO', options),
-        endsAt: new Date(listing.endsAt).toLocaleDateString(
-          'no-NO',
-          optionsWithTime
-        ),
-      };
-    });
-    templates.renderProfilesListings(sorterDate, containerViewLists);
   }
+
+  const sorterDate = listings.map((listing) => {
+    return {
+      ...listing,
+      created: new Date(listing.created).toLocaleDateString('no-NO', options),
+      endsAt: new Date(listing.endsAt).toLocaleDateString(
+        'no-NO',
+        optionsWithTime
+      ),
+    };
+  });
+
+  templates.renderProfilesListings(sorterDate, containerViewLists);
+
   if (bids.length === 0) {
     containerBets.innerHTML = 'No bets yet';
-  } else {
-    const sorterBets = bids.map((bet) => {
-      return {
-        ...bet,
-        created: new Date(bet.created).toLocaleDateString(
-          'no-NO',
-          optionsWithTime
-        ),
-      };
-    });
-    templates.renderProfilesBets(sorterBets, containerBets);
   }
+
+  const sorterBets = bids.map((bet) => {
+    return {
+      ...bet,
+      created: new Date(bet.created).toLocaleDateString(
+        'no-NO',
+        optionsWithTime
+      ),
+    };
+  });
+  templates.renderProfilesBets(sorterBets, containerBets);
 
   if (profile.wins.length === 0) {
     containerWins.innerHTML = 'No wins yet';
-  } else {
-    winsGetListings(profile.wins);
   }
+  winsGetListings(profile.wins);
 
   const name = document.querySelectorAll('.profiles-name');
   const avatar = document.querySelectorAll('.profiles-images');
